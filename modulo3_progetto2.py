cfa220d438bbe1a6bd91d4e94c88c319840f87e7
matteoliotti <matteo260403@gmail.com>
2026-03-28 11:29:33 +0100

    parte 2
    Target Engineering

--- modulo3_progetto2.py
+++ modulo3_progetto2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.datasets import load_linnerud
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 linnerud = load_linnerud()
 X = linnerud.data
@@ -16,3 +17,9 @@ X_std = scaler_X.fit_transform(X)
 
 scaler_y = StandardScaler()
 X_std_y = scaler_y.fit_transform(y)
+
+
+pca_y = PCA(n_components=1)
+y_pca = pca_y.fit_transform(X_std_y)
+
+y_manual = X_std_y[:, 1]
